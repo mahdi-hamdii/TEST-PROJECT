8de564412d44d16e7f7de40663aa0d0785b5df64
Hamdimahdi99 <82580314+Hamdimahdi99@users.noreply.github.com>
2022-06-06 13:25:51 +0100

    e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,10 +3,17 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { SprintService } from '../src/sprint/sprint.service';
+import { DeveloperDto } from '../src/sprint/dto/developer.dto';
+import { DevelopersService } from '../src/sprint/developers.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  const developer = {
+    developername: 'Hamdi.Mahdi',
+    email: 'mahdihamdi@live.fr',
+  };
 
+  let dbDeveloper: DeveloperDto;
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -14,6 +21,9 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    dbDeveloper = await app
+      .get<DevelopersService>(DevelopersService)
+      .findOne({ developername: developer.developername });
   });
 
   it('/ (GET)', () => {
@@ -29,6 +39,27 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect({ sprints: await app.get(SprintService).getAllSprint() });
   });
+
+  it('/api/sprints (POST)', async () => {
+    return request(app.getHttpServer())
+      .post('/api/sprints')
+      .send({
+        name: 'Newly Posted sprint for Test purpose',
+        description: 'Random description',
+        developerId: dbDeveloper.id,
+      })
+      .expect(201)
+      .then((response) => {
+        const payload = response.body;
+        delete payload.id;
+        expect(payload).toStrictEqual({
+          name: 'Newly Posted sprint for Test purpose',
+          description: 'Random description',
+          owner: dbDeveloper,
+        });
+      });
+  });
+
 
 
   afterAll(async () => {
