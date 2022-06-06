e1f7e814216b46d1a4791f2f83efb7b3e49d0347
Hamdimahdi99 <82580314+Hamdimahdi99@users.noreply.github.com>
2022-06-06 13:53:14 +0100

    update main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { SprintModule } from './sprint/sprint.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SprintEntity } from './sprint/entity/sprint.entity';
+import { TaskEntity } from './sprint/entity/task.entity';
+import { DeveloperEntity } from './sprint/entity/developer.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    SprintModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'Hamdi.Mahdi',
+      password: '123456',
+      database: 'devopsDB',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([SprintEntity, TaskEntity, DeveloperEntity]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
